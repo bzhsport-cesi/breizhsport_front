781bffcc11495997586e1934d450ba5efc6218dc
mphel44 <mphel44>
2025-02-07 04:29:56 +0100

    add loading page waiting before test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect, Page } from '@playwright/test';
 
 test('homepage has title', async ({ page }: { page: Page }) => {
-  await page.goto('http://localhost:3000'); // Replace with your app's URL
+  await page.goto('http://localhost:3000');
+  await page.waitForLoadState('load');
   const title = await page.title();
-  expect(title).toBe('Create Next App'); // Replace with your expected title
+  expect(title).toBe('Create Next App');
 });
