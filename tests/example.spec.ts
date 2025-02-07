b607f3ec1eaf1c0a318576a336b073107667ef57
mphel44 <mphel44>
2025-02-07 04:45:38 +0100

    add test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,3 +6,13 @@ test('homepage has title', async ({ page }: { page: Page }) => {
   const title = await page.title();
   expect(title).toBe('Create Next App');
 });
+
+test('homepage renders correctly', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  const mainHeading = await page.locator('h1.text-4xl');
+  await expect(mainHeading).toHaveText('Welcome to Breizh Sports');
+  const subHeading = await page.locator('p.text-lg');
+  await expect(subHeading).toHaveText(
+    'Your one stop shop for all things sports'
+  );
+});
