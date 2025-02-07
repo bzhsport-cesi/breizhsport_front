27786693d4abe4c2806a2ffac0a952c40295b9be
Maxime PHELINE <pheline.maxime@yahoo.fr>
2025-02-07 06:19:23 +0100

    Feature/playwright ci (#3)
    * add playwright test files and corresponding config file
    
    * add Playwright test into Github CI
    
    * fix for test triggering
    
    * add app starting for test CI
    
    * change app starting order into test CI process
    
    * add loading page waiting before test
    
    * fix null check to avoid crash
    
    * add test
    
    * test with yarn
    
    * debug commit
    
    * debug commit
    
    * debug
    
    * d
    
    * dd
    
    * change with starting app
    
    * add no api check in order to access tests
    
    ---------
    
    Co-authored-by: mphel44 <mphel44>

---
+++ playwright.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: 'tests',
+  timeout: 30000,
+  use: {
+    headless: true,
+    actionTimeout: 0,
+    ignoreHTTPSErrors: true,
+    baseURL: 'http://127.0.0.1:3000',
+    trace: 'on-first-retry', // Enable tracing
+  },
+});
