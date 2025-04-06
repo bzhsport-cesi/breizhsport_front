ca8c2df6265e9b13d20ebcdfebb0e48caa5cef9d
Théo Dauzon <theo.dauzon@gmail.com>
2025-04-06 14:26:32 +0200

    refactor: remove unused HTTPS header configuration from Next.js setup

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  swcMinify: true,
-
-  // Ajout d’un header pour informer que la requête originale était en HTTPS
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Forwarded-Proto",
-            value: "https",
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
