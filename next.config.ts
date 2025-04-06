4b804253bd461608bff835203bdf6140021ff389
Théo Dauzon <theo.dauzon@gmail.com>
2025-04-06 13:42:43 +0200

    feat: add HTTPS header support in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  poweredByHeader: false,
+  swcMinify: true,
+
+  // Ajout d’un header pour informer que la requête originale était en HTTPS
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Forwarded-Proto",
+            value: "https",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
