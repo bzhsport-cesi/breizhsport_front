8329676f93bc34b6a0ff18564e94bbfdc7d77d12
Simon <simon.provost@ymail.com>
2025-04-16 16:26:09 +0200

    refactor: improve error handling and response management in ProfilePage and Home components

--- app/api/(auth)/me/route.ts
+++ app/api/(auth)/me/route.ts
@@ -1,5 +1,6 @@
 //app/api/(auth)/me/route.ts
 import { SessionData, sessionOptions } from "@/lib/session";
+import { IUser } from "@/types/types";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
@@ -7,23 +8,28 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     const session = await getIronSession<SessionData>(await cookies(), sessionOptions);
+    let user: IUser | null = null;
 
     if (!session?.jwt) {
         return NextResponse.json(null);
     }
-
-    const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/users/me`, {
-        headers: {
-            Authorization: `Bearer ${session.jwt}`,
-        },
-        cache: "no-store",
-    });
-
-    if (!res.ok) {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/users/me`, {
+            headers: {
+                Authorization: `Bearer ${session.jwt}`,
+            },
+            cache: "no-store",
+        });
+        if (!res.ok) {
+            return NextResponse.json(null);
+        }
+        user = await res.json();
+    } catch (error) {
+        console.error("Error fetching user data:", error);
         return NextResponse.json(null);
     }
 
-    const user = await res.json();
+
     return NextResponse.json(user);
 
 }
